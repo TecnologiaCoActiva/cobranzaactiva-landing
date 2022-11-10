b3cfc2d48d8539d65d40a2703be743d99940c448
AngieeGomez <angiegomezpolo@gmail.com>
2022-11-10 11:32:23 -0500

    [ADD] Header and footer

---
+++ global.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById("menu");
+const menuInfo = document.getElementById("menu_info");
+
+function handleMenu() {
+  menu.classList.toggle("show");
+  menuInfo.classList.toggle("show");
+}
