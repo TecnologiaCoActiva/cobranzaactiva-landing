be830edbb695d2d09fdd2a2fca68682c5e48e1c0
Development team <Andrey@Andrey-Mac.local>
2022-11-21 12:23:12 -0500

    [ADD] Home mobile and meta tags

--- global.js
+++ global.js
@@ -1,7 +1,25 @@
 const menu = document.getElementById("menu");
 const menuInfo = document.getElementById("menu_info");
+const modal = document.querySelector(".modal");
 
-function handleMenu() {
+const handleMenu = () => {
   menu.classList.toggle("show");
   menuInfo.classList.toggle("show");
+};
+
+const showModal = () => {
+  if (sessionStorage.getItem("modal") === null && modal) {
+    modal.classList.add("is-active");
+    sessionStorage.setItem("modal", "true");
+  }
+};
+
+const closeModal = () => {
+  modal.classList.remove("is-active");
+};
+
+if (window.innerWidth > 768) {
+  setTimeout(() => {
+    showModal();
+  }, 2000);
 }
