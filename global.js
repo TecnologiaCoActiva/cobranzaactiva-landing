4b5ebadf29cf31c2b441082606cd8165f571cc54
AngieeGomez <angiegomezpolo@gmail.com>
2023-02-17 19:14:11 -0500

    [UPDATE] Page

--- global.js
+++ global.js
@@ -4,6 +4,11 @@ const menu = document.getElementById("menu");
 const menuOverlay = document.querySelector(".header_overlay");
 const modal = document.querySelector(".modal");
 
+const formatter = new Intl.NumberFormat("es-CO", {
+  currency: "COP",
+  maximumFractionDigits: 0,
+});
+
 const handleMenu = () => {
   menu.classList.toggle("show");
   menuOverlay.classList.toggle("show");
@@ -24,4 +29,30 @@ if (window.innerWidth > 768) {
   setTimeout(async () => {
     showModal();
   }, 2000);
+}
+
+const stats = document.querySelectorAll(".stats span");
+let tresMil = 0;
+let cuatroMil = 0;
+
+if (stats.length > 0) {
+  const handler = (en) => {
+    if (en[0].isIntersecting) {
+      const counter = setInterval(() => {
+        if (tresMil != 3000) {
+          tresMil += 10;
+          stats[0].innerText = `+ $${formatter.format(tresMil)}`;
+        }
+        if (cuatroMil != 4000) {
+          cuatroMil += 10;
+          stats[1].innerText = `+ $${formatter.format(cuatroMil)}`;
+        } else {
+          clearInterval(counter);
+        }
+      }, 10);
+    }
+  };
+
+  const observer = new window.IntersectionObserver(handler);
+  observer.observe(document.querySelector(".stats span"));
 }
